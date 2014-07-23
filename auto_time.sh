fc2d06ad1bf458bfa0571a9edbfb277d200721b4
tgic <farmer1992@gmail.com>
2014-07-24 00:46:58 +0800

    add autotime script

---
+++ auto_time.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function gittime(){
+    git log --pretty=format:%ad --name-only --date=iso8601 $1 | head -n 1
+}
+
+
+for f in `ls -d */`;do 
+    if [ -f $f/Solution.java ];then
+        t=`cat <(gittime $f/Solution.java) <(gittime $f/index.md) | sort -r | head -n 1`
+        sed -i "s/date: .*\$/date: $t/g" $f/index.md
+    fi
+done
