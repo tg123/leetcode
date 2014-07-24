161fb25e6660c55fb8dad5aa820335ab27147a03
tgic <farmer1992@gmail.com>
2014-07-24 22:13:57 +0800

    use readme time

--- auto_time.sh
+++ auto_time.sh
@@ -7,7 +7,7 @@ function gittime(){
 
 for f in `ls -d */`;do 
     if [ -f $f/Solution.java ];then
-        t=`cat <(gittime $f/Solution.java) <(gittime $f/index.md) | sort -r | head -n 1`
+        t=`cat <(gittime $f/Solution.java) <(gittime $f/README.md) | sort -r | head -n 1`
         sed -i "s/date: .*\$/date: $t/g" $f/index.md
     fi
 done
