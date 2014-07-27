a7478e213daca1a77154f4b73cd3313873ced2a4
tgic <farmer1992@gmail.com>
2014-07-28 02:03:17 +0800

    don t cast

--- evaluate-reverse-polish-notation/Solution.java
+++ evaluate-reverse-polish-notation/Solution.java
@@ -1,37 +1,35 @@
 public class Solution {
-    
-    
     public int evalRPN(String[] tokens) {
         
-        
-        final Deque stack = new LinkedList();
+        final Deque<Integer> stack = new LinkedList<Integer>();
         
         for(String t : tokens){
             if("+".equals(t)){
-                int v2 = (int) stack.pop();
-                int v1 = (int) stack.pop();
+                Integer v2 = stack.pop();
+                Integer v1 = stack.pop();
                 
                 stack.push(v1 + v2);
             }else if("-".equals(t)){
-                int v2 = (int) stack.pop();
-                int v1 = (int) stack.pop();
+                Integer v2 = stack.pop();
+                Integer v1 = stack.pop();
                 
                 stack.push(v1 - v2);
             }else if("*".equals(t)){
-                int v2 = (int) stack.pop();
-                int v1 = (int) stack.pop();
+                Integer v2 = stack.pop();
+                Integer v1 = stack.pop();
                 
                 stack.push(v1 * v2);
             }else if("/".equals(t)){
-                int v2 = (int) stack.pop();
-                int v1 = (int) stack.pop();
+                Integer v2 = stack.pop();
+                Integer v1 = stack.pop();
                 
                 stack.push(v1 / v2);
             }else{
-                stack.push(Integer.parseInt(t));
+                stack.push(Integer.valueOf(t));
             }
         }
         
-        return (int)stack.pop();
+        return stack.pop();
+        
     }
 }
