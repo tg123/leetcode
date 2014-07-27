f99a7033b76bb9c014e895457216865aae7dd6ca
tgic <farmer1992@gmail.com>
2014-07-27 23:16:37 +0800

    change preorder to stack

--- binary-tree-preorder-traversal/Solution.java
+++ binary-tree-preorder-traversal/Solution.java
@@ -8,30 +8,57 @@
  * }
  */
 public class Solution {
+    
+    static enum ReturnAddress {
+        PRE, IN, POST, DONE;
+    }
+    
+    static class StackState {
+        ReturnAddress returnAddress = ReturnAddress.PRE;
+        TreeNode param;
+        
+        StackState(TreeNode param){
+            this.param = param;
+        }
+    }
+    
     public ArrayList<Integer> preorderTraversal(TreeNode root) {
         
         ArrayList<Integer> rt = new ArrayList<Integer>();
 
-        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        Deque<StackState> stack = new LinkedList<StackState>();
         
         if(root != null)
-            stack.push(root);
-        
-        //TreeNode current = root;
-        
+            stack.push(new StackState(root));
         
         while(!stack.isEmpty()){
             
-            TreeNode current = stack.pop();
-            rt.add(current.val);
+            StackState current = stack.pop();
             
-            
-            if(current.right != null)
-                stack.push(current.right);
-
-            if(current.left != null)
-                stack.push(current.left);
+            switch(current.returnAddress){
+                case PRE:
+                    current.returnAddress = ReturnAddress.IN;
+                    rt.add(current.param.val);
+                    
+                case IN:
+                    current.returnAddress = ReturnAddress.POST;
+                    
+                    if(current.param.left != null){
+                        stack.push(current);
+                        stack.push(new StackState(current.param.left));
+                        continue;
+                    }
+                case POST:
+                    current.returnAddress = ReturnAddress.DONE;
                 
+                    if(current.param.right != null){
+                        stack.push(current);
+                        stack.push(new StackState(current.param.right));
+                        continue;
+                    }                
+                default:
+                    break;
+            }
         }
         
         
