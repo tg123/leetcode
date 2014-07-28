07a82080db5c822cd0221e177bf958a17d0e3651
tgic <farmer1992@gmail.com>
2014-07-28 19:10:35 +0800

    change trap water to a less brainfuck version

--- trapping-rain-water/Solution.java
+++ trapping-rain-water/Solution.java
@@ -3,46 +3,70 @@ public class Solution {
     static class Bar{
         int pos;
         int height;
+        
+        Bar(int pos, int height){
+            this.pos = pos;
+            this.height = height;
+        }
     }
     
-    public int trap(int[] A) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
+    int containWater(Deque<Bar> queue){
         
-        if(A.length <= 2) return 0;
-        
-        Deque<Bar> stack = new LinkedList<Bar>();
+        Bar left  = queue.peekFirst();
+        Bar right = queue.peekLast();
         
         int water = 0;
         
-        for(int i = 0; i < A.length ; i++){
+        if(right.height >= left.height){
+        
+            water += Math.min(right.height, left.height) * (right.pos - left.pos - 1);
             
-            int bottom = 0;
+            queue.removeFirst(); // remove left
             
-            while(!stack.isEmpty()){
-                Bar left = stack.pop();
-                
-                water += (i - left.pos - 1) * (Math.min(left.height, A[i]) - bottom);
-                
-                bottom = left.height;
-                
-                if(left.height > A[i]){
-                    stack.push(left);
-                    break;
-                }
-                    
+            // remove stones
+            while(queue.size() > 1){
+                water -= queue.removeFirst().height;
             }
-            
-            
-            
-            Bar bar = new Bar();
-            bar.pos = i;
-            bar.height = A[i];
-            
-            stack.push(bar);
-            
         }
         
         return water;
+    }
+    
+    int[] reverseAndToInt(Deque<Bar> queue){
+        int[] a = new int[queue.size()];
+        int i = 0;
+
+        while(!queue.isEmpty()){
+            a[i++] = queue.removeLast().height;
+
+        }
+
+        return a;
+    }
+    
+    public int trap(int[] A) {
         
+        if (A.length <= 2) return 0;
+        
+        Deque<Bar> queue = new LinkedList<Bar>();
+        
+        int s = 0;
+        while(s < A.length && A[s] == 0) s++;
+        
+        if(s < A.length)
+            queue.add(new Bar(s, A[s]));
+        
+        int water = 0;
+        
+        for(int i = s + 1; i < A.length; i++){
+            
+            queue.add(new Bar(i, A[i]));
+            
+            water += containWater(queue);
+        }
+        
+        water += trap(reverseAndToInt(queue));
+        
+        return water;
     }
 }
