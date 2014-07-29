ed44cc1b7fc93fdf9e09da7081446a10c101a380
tgic <farmer1992@gmail.com>
2014-07-30 01:24:33 +0800

    refine code

--- pascals-triangle-ii/Solution.java
+++ pascals-triangle-ii/Solution.java
@@ -1,17 +1,15 @@
 public class Solution {
-    public ArrayList<Integer> getRow(int rowIndex) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
+    public List<Integer> getRow(int rowIndex) {
         Integer[] row = new Integer[rowIndex + 1];
         
-        //row[0] = 1;
-        //row[rowIndex] = 1;
         Arrays.fill(row, 1);
         
-        for(int i = 0 ; i < rowIndex - 1; i++)
-        for(int j = i + 1; j >= 1; j--){
-            row[j] = row[j] + row[j - 1];
+        for(int i = 0 ; i < rowIndex - 1; i++){
+            for(int j = i + 1; j >= 1; j--){
+                row[j] = row[j] + row[j - 1];
+            }
         }
         
-        return new ArrayList<Integer>(Arrays.asList(row));
+        return Arrays.asList(row);
     }
 }
