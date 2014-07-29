05440b2321e6625b2f5662c48b60c45b5faea2af
tgic <farmer1992@gmail.com>
2014-07-30 00:35:23 +0800

    refine the code

--- best-time-to-buy-and-sell-stock/Solution.java
+++ best-time-to-buy-and-sell-stock/Solution.java
@@ -1,16 +1,15 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
         
-        if(prices.length == 0 )return 0;
+        int max = 0;
         
-        int max = Integer.MIN_VALUE;
-        int start = 0;
+        int lowest = Integer.MAX_VALUE;
         
-        for(int i = 0; i < prices.length; i++){
-            if(prices[start] > prices[i]) start = i;
-            int b = prices[i] - prices[start];
-            if(b > max) max = b;
+        for(int p : prices){
+    
+            lowest = Math.min(lowest, p);
+            
+            max = Math.max(p - lowest, max);
         }
         
         return max;
