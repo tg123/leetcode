0fd6cb3d6c3f9a5dee5c896a373bdeda731970fd
tgic <farmer1992@gmail.com>
2014-07-30 00:21:28 +0800

    refine the code

--- longest-valid-parentheses/Solution.java
+++ longest-valid-parentheses/Solution.java
@@ -1,35 +1,33 @@
 public class Solution {
-    public int longestValidParentheses(String ss) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
+    public int longestValidParentheses(String s) {
+        if(s == null) return 0;
         
-        if(ss == null) return 0;
-        
-        char[] chars = ss.toCharArray();
+        char[] chars = s.toCharArray();
         if(chars.length <= 1) return 0;
         
-        LinkedList<Character> s = new LinkedList<Character>();
+        LinkedList<Character> stack = new LinkedList<Character>();
         
-        int[] max = new int[chars.length];
-        int len = 0;
+        int[] count = new int[chars.length];
+        int max = 0;
+        
         for(int i = 0 ; i<chars.length; i++ ){
             
-            Character _ins = s.peek();
-            char ins = _ins != null ? _ins : ' ';
-            char cur = chars[i];
+            Character ins = stack.peek();
+            Character cur = chars[i];
              
-            if(ins == '(' && cur == ')'){
-                s.pop();
-                max[s.size()] += 2 + max[s.size() + 1];
-                max[s.size() + 1] = 0;
+            if(ins != null && ins == '(' && cur == ')'){
+                stack.pop();
+                
+                count[stack.size()] += 2 + count[stack.size() + 1];
+                count[stack.size() + 1] = 0;
+                
+                max = Math.max(max, count[stack.size()]);
+                
             }else{
-              s.push(cur);  
+                stack.push(cur);  
             }
         }
         
-        int _max = 0;
-        for(int i = 0; i<max.length; i++)
-            if(max[i] > _max) _max = max[i];
-        
-        return _max;
+        return max;
     }
 }
