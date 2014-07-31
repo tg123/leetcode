59bce7b92bcd6fcd3b6bcab11e54a304a806b388
tgic <farmer1992@gmail.com>
2014-08-01 01:36:11 +0800

    change to list

--- spiral-matrix/Solution.java
+++ spiral-matrix/Solution.java
@@ -1,7 +1,5 @@
 public class Solution {
-    public ArrayList<Integer> spiralOrder(int[][] matrix) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        
+    public List<Integer> spiralOrder(int[][] matrix) {
         ArrayList<Integer> rt = new ArrayList<Integer>();
         
         if(matrix.length == 0) return rt;
@@ -9,21 +7,17 @@ public class Solution {
         int x = 0, y = 0, my = matrix.length, mx = matrix[0].length;
         //int dx = 1, dy = 1;
         
-        
-        
         while(x < mx && y < my){
             for(int i = x; i < mx && y < my ; i++){
                 rt.add(matrix[y][i]);
             }
             
-            //dx *= -1;
             y++;
             
             for(int i = y; i< my && x < mx; i++){
                 rt.add(matrix[i][mx - 1]);
             }
             
-            //dy *= -1;
             mx--;
             
             for(int i = mx - 1; i >= x && y < my ; i--){
@@ -42,5 +36,6 @@ public class Solution {
         
         
         return rt;
+        
     }
 }
