f8bb583a3027724e14120bcb2e46bde71e53a07f
tgic <farmer1992@gmail.com>
2014-07-31 14:11:55 +0800

    refine the code

--- valid-palindrome/Solution.java
+++ valid-palindrome/Solution.java
@@ -1,25 +1,29 @@
 public class Solution {
+    
+    boolean valid(char[] chars, int i){
+        return !(('A' <= chars[i] && chars[i]<= 'Z') 
+                || ('a' <= chars[i] && chars[i]<= 'z') 
+                || ('0' <= chars[i] && chars[i]<= '9'));
+    }
+    
     public boolean isPalindrome(String s) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
         if(s == null) return false;
-        
         
         char[] chars = s.toLowerCase().toCharArray();
         int i = 0, j = chars.length - 1;
+        
         while(i < j){
-            while( (i < j) && !(('A' <= chars[i] && chars[i]<= 'Z') || ('a' <= chars[i] && chars[i]<= 'z') || ('0' <= chars[i] && chars[i]<= '9')  )  ) i++;
             
-            while( (i < j) && !(('A' <= chars[j] && chars[j]<= 'Z') || ('a' <= chars[j] && chars[j]<= 'z') || ('0' <= chars[j] && chars[j]<= '9')  )  ) j--;
+            // ignore space etc.
+            while( (i < j) && valid(chars, i)) i++;
+            while( (i < j) && valid(chars, j)) j--;
             
             if(chars[i] != chars[j]) return false;
             
             i++;
             j--;
-            
-            
         }
         
         return true;
-        
     }
 }
