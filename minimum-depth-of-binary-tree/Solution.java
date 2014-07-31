5c3f24b5cab7ee3b83bc336a15ae0d9d2e24065d
tgic <farmer1992@gmail.com>
2014-08-01 01:24:15 +0800

    refine the code

--- minimum-depth-of-binary-tree/Solution.java
+++ minimum-depth-of-binary-tree/Solution.java
@@ -8,16 +8,15 @@
  * }
  */
 public class Solution {
-    
     public int minDepth(TreeNode root) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
         
         if(root == null) return 0;
-        else if(root.left == null && root.right == null) return 1; //leaf
-        else{
-            int ml = root.left != null ? minDepth(root.left) : Integer.MAX_VALUE;
-            int rl = root.right != null ? minDepth(root.right) : Integer.MAX_VALUE;
-            return Math.min(rl, ml) + 1;
-        }
+        if(root.left == null && root.right == null) return 1;
+        
+        if(root.left != null && root.right == null) return minDepth(root.left)  + 1;
+        if(root.left == null && root.right != null) return minDepth(root.right) + 1;
+        
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        
     }
 }
