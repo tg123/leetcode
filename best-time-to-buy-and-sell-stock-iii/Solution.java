8259e781c1cfb5826ad5b2f5664393cafdf7eabb
tgic <farmer1992@gmail.com>
2014-08-02 22:18:59 +0800

    fix typo

--- best-time-to-buy-and-sell-stock-iii/Solution.java
+++ best-time-to-buy-and-sell-stock-iii/Solution.java
@@ -1,7 +1,5 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        
-        
         if(prices.length < 2) return 0;
         
         int[] left = new int[prices.length];
@@ -12,14 +10,14 @@ public class Solution {
         
         for(int i = 1; i < prices.length; i++){
             left_min = Math.min(left_min, prices[i]);
-            left[i] = Math.max(prices[i] - left_min, left[i - 1]);
+            left[i]  = Math.max(prices[i] - left_min, left[i - 1]);
         }
         
-        int rigth_max = prices[prices.length - 1];
+        int right_max = prices[prices.length - 1];
         
         for(int i = prices.length - 2; i >= 0; i--){
-            rigth_max = Math.max(rigth_max, prices[i]);
-            right[i] = Math.max(rigth_max - prices[i], right[i + 1]);
+            right_max = Math.max(right_max, prices[i]);
+            right[i]  = Math.max(right_max - prices[i], right[i + 1]);
         }
         
         int m = 0;
