aeacfeda49040c311ffcc3330ac46efaac1a24e5
tgic <farmer1992@gmail.com>
2014-08-03 09:04:52 +0800

    change to list

--- generate-parentheses/Solution.java
+++ generate-parentheses/Solution.java
@@ -1,22 +1,22 @@
 public class Solution {
-    public ArrayList<String> generateParenthesis(int n) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
+    public List<String> generateParenthesis(int n) {
         
         if (n == 0) return new ArrayList<String>();
-        if (n == 1) return new ArrayList<String>(Arrays.asList(new String[]{"()"}));
+        if (n == 1) return Arrays.asList(new String[]{"()"});
         
         HashSet<String> temp = new HashSet<String>();
         
-        for(int i = 1; i < n ; i++)
-        for(String s : generateParenthesis(n - i)){
-            if(i == 1) temp.add("(" + s + ")");
-            
-            for(String ss : generateParenthesis(i)){
-                          
-                temp.add(ss + s);
-                temp.add(s + ss);
+        for(int i = 1; i < n ; i++){
+            for(String s : generateParenthesis(n - i)){
+                if(i == 1) temp.add("(" + s + ")");
+                
+                for(String ss : generateParenthesis(i)){
+                              
+                    temp.add(ss + s);
+                    temp.add(s + ss);
+                }
+      
             }
-  
         }
         
         return new ArrayList<String>(temp);
