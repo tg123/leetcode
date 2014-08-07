9cf332069b3f80f17d490d90f3283be2631eaf7d
tgic <farmer1992@gmail.com>
2014-08-07 14:22:53 +0800

    more readable

--- maximum-subarray/Solution.java
+++ maximum-subarray/Solution.java
@@ -1,18 +1,20 @@
 public class Solution {
     public int maxSubArray(int[] A) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        
-        //  max ( x + A[i] , x)
         
         int max = A[0];
-        int x = A[0];
+        int history = A[0];
         
         for(int i = 1; i < A.length; i++){
-            x = Math.max(A[i], x + A[i]);
-            max = Math.max(max, x);
+            if(history < 0){
+                history = A[i];
+            }else{
+                history += A[i];
+            }
+            
+            max = Math.max(max, history);
+            
         }
         
         return max;
-        
     }
 }
