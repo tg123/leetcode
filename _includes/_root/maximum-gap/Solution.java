10270b7822951456a54ba1f0b779674a5571e07e
tgic <farmer1992@gmail.com>
2014-12-15 02:26:25 +0800

    commit max gap to gh-pages

--- _includes/_root/maximum-gap/Solution.java
+++ _includes/_root/maximum-gap/Solution.java
@@ -4,66 +4,49 @@ public class Solution {
     int min = Integer.MAX_VALUE;
     int max = Integer.MIN_VALUE;
 
-    int[] nums = new int[0];
-
     void add(int n){
       min = Math.min(n, min);
       max = Math.max(n, max);
-
-
-      nums = Arrays.copyOf(nums, nums.length + 1);
-
-      nums[nums.length - 1] = n;
-    }
-
-    int gap(){
-      if (nums.length == 1) return 0;
-      if (nums.length == 2) return max - min;
-
-      // bad case
-      Arrays.sort(nums);
-
-      int g = nums[1] - nums[0];
-      for(int i = 2; i < nums.length; i++){
-        g = Math.max(nums[i] - nums[i - 1], g);
-      }
-
-      return g;
     }
   }
 
   public int maximumGap(int[] num) {
     if (num.length < 2) return 0;
 
-    double _avg = num[0];
+    int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
 
     for(int i = 0; i < num.length; i++){
-      _avg += (num[i] - _avg) / (i + 1);
+      max = Math.max(max, num[i]);
+      min = Math.min(min, num[i]);
     }
 
-    int avg = (int) Math.ceil(_avg);
 
-    Bucket[] buckets = new Bucket[num.length];
+    int gap = (int) Math.ceil((double)(max - min) / (num.length - 1));
+    int n   = (max - min) / gap + 1;
+
+    Bucket[] buckets = new Bucket[n];
+
     for(int i = 0; i < num.length; i++){
-      int index = num[i] / avg;
+      int index = (num[i] - min) / gap;
 
       if(buckets[index] == null) buckets[index] = new Bucket();
 
       buckets[index].add(num[i]);
     }
 
-    int gap = Integer.MIN_VALUE;
+    int maxGap = Integer.MIN_VALUE;
 
-    if (buckets[0] != null) gap = buckets[0].gap();
+    int prev   = min;
 
-    for(int i = 1; i < buckets.length; i++){
+    for(int i = 0; i < buckets.length; i++){
       if(buckets[i] == null) continue;
-      gap = Math.max(gap, buckets[i].gap());
 
-      if(buckets[i - 1] == null) continue;
-      gap = Math.max(gap, buckets[i].min - buckets[i - 1].max);
+      maxGap = Math.max(maxGap, buckets[i].min - prev);
+
+      prev   = buckets[i].max;
     }
 
-    return gap;
+    return maxGap;
   }
 }
