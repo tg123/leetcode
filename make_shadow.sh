ca4efcd1fc54ebec737df5627202756c839e654c
tgic <farmer1992@gmail.com>
2015-01-19 00:47:47 +0800

    make_shadow is no longer need

--- make_shadow.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for f in `ls -d */`;do 
-    if [ -f $f/Solution.java ];then
-        mkdir -p _includes/_root/$f
-        /bin/cp -r $f/Solution.java _includes/_root/$f/Solution.java
-        /bin/cp -r $f/README.md _includes/_root/$f/README.md
-    fi
-done
