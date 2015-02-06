b2929efd9dbecd53ee73995b23e76bc892c7efa1
tgic <farmer1992@gmail.com>
2015-02-07 02:08:09 +0800

    fix some hardcode

--- repeated-dna-sequences/Solution.java
+++ repeated-dna-sequences/Solution.java
@@ -1,69 +1,64 @@
 public class Solution {
+    
+    static final int SIZE = 10;
+    static final char[] CHAR_TO_INT = {'A', 'C', 'G', 'T'};
+    static final int[]  INT_TO_CHAR = new int['T' + 1];
+    static final int[]  POW = new int[SIZE];
 
-  static final int SIZE = 10;
-  static final char[] CHAR_TO_INT = {'A', 'C', 'G', 'T'};
-  static final int[] INT_TO_CHAR = new int['T' + 1];
-  static final int[] POW = new int[SIZE];
+    static {
+        for(int i = 0; i < SIZE; i++){
+            POW[i] = (int) Math.pow(4, i);
+        }
 
-  static {
-    for(int i = 0; i < SIZE; i++){
-      POW[i] = (int) Math.pow(4, i);
+        INT_TO_CHAR['A'] = 0;
+        INT_TO_CHAR['C'] = 1;
+        INT_TO_CHAR['G'] = 2;
+        INT_TO_CHAR['T'] = 3;
     }
 
-    INT_TO_CHAR['A'] = 0;
-    INT_TO_CHAR['C'] = 1;
-    INT_TO_CHAR['G'] = 2;
-    INT_TO_CHAR['T'] = 3;
-  }
+    int toInt(char[] s, int index){
+        if(index + SIZE > s.length){
+            return -1;
+        }
 
-  int toInt(char[] s, int index){
-    if(index + 10 > s.length){
-      return -1;
+        int n = 0;
+        for(int i = index; i < index + SIZE; i++){
+            n += INT_TO_CHAR[s[i]] * POW[i - index];
+        }
+
+        return n;
     }
 
-    int n = 0;
-    for(int i = index; i < index + 10; i++){
-      n += INT_TO_CHAR[s[i]] * POW[i - index];
+    String formInt(int n){
+        char[] s = new char[SIZE];
+
+        for(int i = 0; i < SIZE; i++){
+            s[i] = CHAR_TO_INT[n % 4];
+            n /= 4;
+        }
+        return new String(s);
     }
 
-    return n;
-  }
 
-  String formInt(int n){
+    public List<String> findRepeatedDnaSequences(String s) {
 
-    char[] s = new char[10];
+        char[] S = s.toCharArray();
 
-    for(int i = 0; i < 10; i++){
-      int v = 4;
+        final int max = toInt("TTTTTTTTTT".toCharArray(), 0);
+        int[] m = new int[max + 1];
 
-      s[i] = CHAR_TO_INT[n % v];
+        for(int i = 0; i <= S.length - SIZE; i++){
+            m[toInt(S, i)]++;
+        }
 
-      n /= v;
+        ArrayList<String> rt = new ArrayList<>();
 
+        for(int i = 0; i < max + 1; i++){
+            if(m[i] >= 2){
+                rt.add(formInt(i));
+            }
+        }
+
+        return rt;
     }
-    return new String(s);
-  }
-
-
-  public List<String> findRepeatedDnaSequences(String s) {
-
-    char[] S = s.toCharArray();
-
-    final int max = toInt("TTTTTTTTTT".toCharArray(), 0);
-    int[] m  = new int[max];
-
-    for(int i = 0; i <= S.length - 10; i++){
-      m[toInt(S, i)]++;
-    }
-
-    ArrayList<String> rt = new ArrayList<>();
-
-    for(int i = 0; i < max; i++){
-      if(m[i] >= 2){
-        rt.add(formInt(i));
-      }
-    }
-
-    return rt;
-  }
 }
