14ad29ce0b0c0db5e21e84ad8dc7d996726087e1
tgic <farmer1992@gmail.com>
2015-02-13 00:41:10 +0800

    java 8 style

--- gray-code/Solution.java
+++ gray-code/Solution.java
@@ -1,15 +1,10 @@
 public class Solution {
-    public ArrayList<Integer> grayCode(int n) {
+    public List<Integer> grayCode(int n) {
         
-        ArrayList<Integer> rt = new ArrayList<Integer>();
-        
-        
-        for(int i = 0; i < Math.pow(2, n); i++){
-            // G(N) = (B(n)/2) XOR B(n)
-            rt.add((i / 2) ^ i);
-        }
-        
-        return rt;
+        return IntStream.range(0, (int) Math.pow(2, n))
+            .map(i -> (i >> 1) ^ i )
+            .boxed()
+            .collect(Collectors.toList());
         
     }
 }
