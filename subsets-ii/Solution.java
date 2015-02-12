0773f75ad4e943f2967cbc82f3f4f4f220736f51
tgic <farmer1992@gmail.com>
2015-02-13 01:14:40 +0800

    java 8 style

--- subsets-ii/Solution.java
+++ subsets-ii/Solution.java
@@ -1,49 +1,15 @@
 public class Solution {
     public List<List<Integer>> subsetsWithDup(int[] num) {
-        
-        ArrayList<List<Integer>> rt = new ArrayList<List<Integer>>();
-        
-        if (num.length == 0){
-            rt.add(new ArrayList<Integer>());
-            
-            return rt;
-        }
-        
         Arrays.sort(num);
-        
-        long mask = (long) Math.pow(2, num.length);
-        
-        next:
-        for(long i = 0; i < mask; i++){
-            
-            ArrayList<Integer> level = new ArrayList<Integer>();
-            
-            for(long j = 0; j < num.length; j++){
-                
-                long x = (long) Math.pow(2, j);
-                
-                if((x & i) == x){
-                    level.add(num[(int)j]);
-                }
-            }
-            
-            next_lv:
-            for(List<Integer> _level : rt){
-                
-                if(_level.size() == level.size()){
-                    for(int li = 0; li < _level.size(); li++){
-                        if(!_level.get(li).equals(level.get(li)))
-                            continue next_lv;
-                    }
-                    
-                    continue next;   
-                }
-            }
-            rt.add(level);
-            
-        }
-        
-        return rt;
-        
+
+        return new ArrayList<>(IntStream.range(0, 1 << num.length)
+                .mapToObj(mask ->
+                                IntStream.range(0, num.length)
+                                        .filter(i -> ((1 << i) & mask) > 0)
+                                        .map(i -> num[i])
+                                        .boxed()
+                                        .collect(Collectors.toList())
+                )
+                .collect(Collectors.toSet()));        
     }
 }
