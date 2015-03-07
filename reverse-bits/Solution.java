9807b1a4d4acba31d011063c46f823481375c03c
tgic <farmer1992@gmail.com>
2015-03-07 19:36:21 +0800

    add reverse bits

---
+++ reverse-bits/Solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+
+    // from JDK Integer.reverse
+    
+    /**
+     * Returns the value obtained by reversing the order of the bits in the
+     * two's complement binary representation of the specified {@code int}
+     * value.
+     *
+     * @param i the value to be reversed
+     * @return the value obtained by reversing order of the bits in the
+     *     specified {@code int} value.
+     * @since 1.5
+     */
+    public static int reverse(int i) {
+        // HD, Figure 7-1
+        i = (i & 0x55555555) << 1 | (i >>> 1) & 0x55555555;
+        i = (i & 0x33333333) << 2 | (i >>> 2) & 0x33333333;
+        i = (i & 0x0f0f0f0f) << 4 | (i >>> 4) & 0x0f0f0f0f;
+        i = (i << 24) | ((i & 0xff00) << 8) |
+            ((i >>> 8) & 0xff00) | (i >>> 24);
+        return i;
+    }
+    
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        return reverse(n);
+    }
+}
---
+++ reverse-bits/index.md
@@ -0,0 +1,9 @@
+---
+layout: solution
+title: Reverse Bits
+date: 2015-03-07 19:33:34+08:00
+leetcode_id: 190
+---
+{% assign leetcode_name = {{page.path | remove: '/index.md'}}  %}
+{% assign leetcode_readme = {{leetcode_name | append: '/README.md' | prepend: '_root/' }}  %}
+{% include {{leetcode_readme}} %}
