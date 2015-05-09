14dba2d874a2671fc0e99d61d258a1c821b06ee9
tgic <farmer1992@gmail.com>
2015-05-09 22:51:01 +0800

    renew code

--- powx-n/Solution.java
+++ powx-n/Solution.java
@@ -1,17 +1,14 @@
 public class Solution {
-    public double pow(double x, int n) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
+    public double myPow(double x, int n) {
+        
         if(n == 0) return 1;
         
-        double s = pow(x, n /2);
+        double s = myPow(x, n / 2);
         
-        if(n % 2 == 0)
-        return s * s;
-        else if(n > 0)
-        return s * s * x;
-        else 
+        if(n % 2 == 0) return s * s;
+        
+        if(n > 0) return s * s * x;
+        // else n < 0
         return s * s / x;
-        
-        
     }
 }
