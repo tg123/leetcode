cdf978d490f9b850fee719672bde7d61453fb92e
tgic <farmer1992@gmail.com>
2015-05-11 01:12:33 +0800

    optimize code

--- bitwise-and-of-numbers-range/Solution.java
+++ bitwise-and-of-numbers-range/Solution.java
@@ -1,12 +1,16 @@
 public class Solution {
-    public int rangeBitwiseAnd(int m, int n) {
-        final int SIZE = Integer.SIZE;
+    
+    static final int SIZE = Integer.SIZE;
 
-        long[] POW = new long[SIZE + 1];
+    static final long[] POW = new long[SIZE + 1];
 
+    static {
         for(int i = 0; i < SIZE; i++){
             POW[i] = (long)Math.pow(2, i);
-        }
+        }        
+    }
+    
+    public int rangeBitwiseAnd(int m, int n) {
 
         for(int i = SIZE; i > 0; i--){
             if(POW[i - 1] <= m && m < POW[i]){
