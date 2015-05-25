e90d4f26e0f018d6143c0d04c879537339804750
tgic <farmer1992@gmail.com>
2015-05-26 01:33:37 +0800

    ... not clear with this problem

---
+++ kth-largest-element-in-an-array/Solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        return nums[nums.length - k];
+    }
+}
---
+++ kth-largest-element-in-an-array/index.md
@@ -0,0 +1,7 @@
+---
+layout: solution
+title: Kth Largest Element in an Array
+date: 2015-05-26 01:30:24+08:00
+leetcode_id: 215
+---
+{% include_relative README.md %}
