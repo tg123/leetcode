6a62a7d8a7cd3825621e8f9c2fad8c9493ec80d4
tgic <farmer1992@gmail.com>
2015-05-31 01:01:13 +0800

    a minheap version

--- kth-largest-element-in-an-array/Solution.java
+++ kth-largest-element-in-an-array/Solution.java
@@ -1,7 +1,83 @@
 public class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
+    
+    static class MinHeap {
+        
+        int[] data;
+        
+        int currentSize = 0;
+        
+        MinHeap(int size){
+            data = new int[size];    
+        }
+        
+        int parent(int i){
+            return (i - 1) / 2;
+        }
+        
+        int left(int i){
+            return 2 * i + 1;
+        }
+        
+        int right(int i){
+            return 2 * i + 2;
+        }
 
-        return nums[nums.length - k];
+        void add(int n){
+            int i = currentSize;
+            currentSize++;
+            
+            data[i] = n;
+            
+            while(i > 0 && data[parent(i)] > data[i]){
+                swap(parent(i), i);
+                i = parent(i);
+            }
+        }        
+        
+        void heapify(int i){
+            
+            int l   = left(i);
+            int r   = right(i);
+            int min = i;
+            
+            if(l < currentSize && data[l] < data[min]){
+                min = l;
+            }
+            
+            if(r < currentSize && data[r] < data[min]){
+                min = r;
+            }
+            
+            if(min != i){
+                swap(i, min);
+                heapify(min);
+            }
+            
+        }
+        
+        void swap(int x, int y){
+            int t   = data[x];
+            data[x] = data[y];
+            data[y] = t;
+        }
+    }
+    
+    public int findKthLargest(int[] nums, int k) {
+        
+        MinHeap heap = new MinHeap(k);
+        
+        for(int i = 0; i < Math.min(nums.length, k); i++){
+            heap.add(nums[i]);    
+        }
+        
+        for(int i = k; i < nums.length; i++){
+            
+            if(heap.data[0] < nums[i]){
+                heap.data[0] = nums[i];
+                heap.heapify(0);
+            }
+        }
+        
+        return heap.data[0];
     }
 }
